2fb31167267988b6042876359ecc7c4d9198428a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-28 02:56:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,19 +7,19 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    
+
   end
 
   get '/square/:number' do
-    
+
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    
+
   end
 
   get '/:operation/:number1/:number2' do
-    
+
   end
-  
+
 end
